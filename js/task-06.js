d21126e11524fcb0132860983790fe21908f9430
kristinaordenans <okristin92@gmail.com>
2023-03-24 09:32:16 +0200

    6

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,17 @@
 const formInput = document.querySelector("#validation-input");
+const dataLength = Number(formInput.getAttribute('data-length'));
+
+// console.log(formInput)
+
 formInput.addEventListener('blur', (event) => {
-    if(event.target.value.trim().length === Number(formInput.getAttribute('data-length'))) {
-        event.target.style.outline = '2px solid #4caf50';
-        return;
-    } 
-    event.target.style.outline = '2px solid #f44336' ;
+    if (event.target.value.length === dataLength) {
+        formInput.classList.add('valid')
+        formInput.classList.remove('invalid')
+    } else if (event.target.value.length === 0) {
+        formInput.classList.remove('valid')
+        formInput.classList.remove('invalid')
+    } else {
+        formInput.classList.add('invalid')
+        formInput.classList.remove('valid')
+    }
 });
